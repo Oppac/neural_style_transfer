00773700f4814d341109fdea06d39736eb1f126d
Oppac <oppac26@gmail.com>
2021-04-08 17:03:20 +0200

    optim

--- nst.py
+++ nst.py
@@ -1,28 +1,38 @@
 import matplotlib.pyplot as plt
+from PIL import Image
+
 import torch
 import torchvision
 from torchvision import models, transforms
-from PIL import Image
 
 import nst_vgg
 
-def show_img(processed_img):
-    unloader = transforms.Compose([
-        transforms.Normalize(
-            mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
-            std=[1/0.229, 1/0.224, 1/0.225]),
-        transforms.ToPILImage()
-        ])
+def show_img(processed_img, denorm=True):
+    if denorm:
+        unloader = transforms.Compose([
+            transforms.Normalize(
+                mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
+                std=[1/0.229, 1/0.224, 1/0.225]),
+            transforms.ToPILImage()
+            ])
+    else:
+        unloader = transforms.ToPILImage()
     img = unloader(processed_img.squeeze(0))
     plt.imshow(img)
     plt.show()
+
+def gram_matrix(tensor):
+    _, channels, height, width = tensor.size()
+    tensor = tensor.view(channels, height * width)
+    gram = torch.mm(tensor, tensor.t())
+    return gram
 
 if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     transform = transforms.Compose([
-        transforms.Resize(256),
+        transforms.Resize((400, 400)),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
@@ -31,20 +41,54 @@ if __name__ == "__main__":
 
     content_img = Image.open("images\jade.jpg")
     content_img = transform(content_img).unsqueeze(0).to(device)
-    target_img = torch.randn_like(content_img).requires_grad_(True).to(device)
-    #show_img(target_img)
+
+    style_img = Image.open("images\starry_night.jpg")
+    style_img = transform(style_img).unsqueeze(0).to(device)
+
+    output_img = torch.randn_like(content_img).requires_grad_(True).to(device)
+    #output_img = content_img.clone().requires_grad_(True).to(device)
 
     model = nst_vgg.Vgg19Nst().to(device).eval()
-    optimizer = torch.optim.Adam([target_img], lr=0.01)
-    mse_loss = torch.nn.MSELoss(reduction='mean')
+    content_maps = model(content_img)
+    style_maps = model(style_img)
 
-    for i in range(1, 1000):
+    content_layers = ['conv4_2']
+    style_layers = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
+    content_rep = {layer: content_maps[layer].squeeze(axis=0) for layer in content_layers}
+    style_grams = {layer: gram_matrix(style_maps[layer]) for layer in style_layers}
+
+    optimizer = torch.optim.LBFGS([output_img], max_iter=3000)
+    mse_loss_content = torch.nn.MSELoss(reduction='mean')
+    mse_loss_style = torch.nn.MSELoss(reduction='sum')
+
+    run = 0
+    def closure():
+        global run
         optimizer.zero_grad()
-        loss = mse_loss(model(content_img)['conv4_2'], model(target_img)['conv4_2'])
-        loss.backward(retain_graph=True)
-        optimizer.step()
 
-        if i%50 == 0:
-            print(loss)
+        content_loss = 0
+        content_weights = 1
+        style_loss = 0
+        style_weights = 10
+        output_rep = model(output_img)
 
-    show_img(target_img)
+        for name in content_layers:
+            content_loss += mse_loss_content(content_rep[name], output_rep[name].squeeze(axis=0))
+        content_loss /= len(content_layers)
+
+        output_grams = {layer: gram_matrix(output_rep[layer]) for layer in style_layers}
+        for name in style_layers:
+            style_loss += mse_loss_style(style_grams[name], output_grams[name])
+        style_loss /= len(style_layers)
+
+        total_loss = content_weights * content_loss + style_weights * style_loss
+        total_loss.backward()
+        print(f"total_loss: {total_loss} | content_loss: {content_loss} | style_loss: {style_loss}")
+
+        run += 1
+        return total_loss
+
+    optimizer.step(closure)
+
+
+    show_img(output_img)
