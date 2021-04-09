f6065dfef7a76cc3a24d0aef04b1c1f9ced45bc6
Oppac <oppac26@gmail.com>
2021-04-09 17:56:10 +0200

    content_bug

--- nst.py
+++ nst.py
@@ -36,17 +36,17 @@ if __name__ == "__main__":
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225])
-        ])
+            std=[0.229, 0.224, 0.225]
+        )])
 
-    content_img = Image.open("images\jade.jpg")
+    content_img = Image.open("images\candy.jpg")
     content_img = transform(content_img).unsqueeze(0).to(device)
 
-    style_img = Image.open("images\starry_night.jpg")
+    style_img = Image.open("images\\ny.jpg")
     style_img = transform(style_img).unsqueeze(0).to(device)
 
-    output_img = torch.randn_like(content_img).requires_grad_(True).to(device)
-    #output_img = content_img.clone().requires_grad_(True).to(device)
+    #output_img = torch.randn_like(content_img).requires_grad_(True).to(device)
+    output_img = content_img.clone().requires_grad_(True).to(device)
 
     model = nst_vgg.Vgg19Nst().to(device).eval()
     content_maps = model(content_img)
@@ -57,38 +57,41 @@ if __name__ == "__main__":
     content_rep = {layer: content_maps[layer].squeeze(axis=0) for layer in content_layers}
     style_grams = {layer: gram_matrix(style_maps[layer]) for layer in style_layers}
 
-    optimizer = torch.optim.LBFGS([output_img], max_iter=3000)
+    optimizer = torch.optim.LBFGS([output_img])
     mse_loss_content = torch.nn.MSELoss(reduction='mean')
     mse_loss_style = torch.nn.MSELoss(reduction='sum')
 
     run = 0
-    def closure():
-        global run
-        optimizer.zero_grad()
+    while run <= 3:
+        def closure():
+            global run
+            optimizer.zero_grad()
 
-        content_loss = 0
-        content_weights = 1
-        style_loss = 0
-        style_weights = 10
-        output_rep = model(output_img)
+            content_loss = 0
+            content_weights = 1
+            style_loss = 0
+            style_weights = 1
+            output_rep = model(output_img)
 
-        for name in content_layers:
-            content_loss += mse_loss_content(content_rep[name], output_rep[name].squeeze(axis=0))
-        content_loss /= len(content_layers)
+            for name in content_layers:
+                content_loss += mse_loss_content(content_rep[name], output_rep[name].squeeze(axis=0))
+            content_loss /= len(content_layers)
 
-        output_grams = {layer: gram_matrix(output_rep[layer]) for layer in style_layers}
-        for name in style_layers:
-            style_loss += mse_loss_style(style_grams[name], output_grams[name])
-        style_loss /= len(style_layers)
+            output_grams = {layer: gram_matrix(output_rep[layer]) for layer in style_layers}
+            for name in style_layers:
+                style_loss += mse_loss_style(style_grams[name], output_grams[name])
+            style_loss /= len(style_layers)
 
-        total_loss = content_weights * content_loss + style_weights * style_loss
-        total_loss.backward()
-        print(f"total_loss: {total_loss} | content_loss: {content_loss} | style_loss: {style_loss}")
+            total_loss = (content_weights * content_loss) + (style_weights * style_loss)
+            total_loss.backward()
 
-        run += 1
-        return total_loss
+            if run%5 == 0:
+                print(f"total_loss: {total_loss} | content_loss: {content_weights * content_loss} | style_loss: {style_weights * style_loss}")
 
-    optimizer.step(closure)
+            run += 1
+            return total_loss
 
+        optimizer.step(closure)
 
+    output_img.data.clamp_(-1.5, 1.5)
     show_img(output_img)
